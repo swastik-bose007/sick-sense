b56b3144f470072f6d028e00baa5283eeb040032
Chirantan Das <daschirantan0002@gmail.com>
2024-01-19 13:26:47 +0530

    Add nativewind/babel plugin and update dependencies

---
+++ src/config/colorTheme.js
@@ -0,0 +1,12 @@
+export default {
+    darkGreen: '#003128',
+    fernGreen: '#4F772D',
+    keppal: '#69AFA1',
+    ashGrey: '#ADD0CA',
+    mintCream: '#E8FAF4',
+    champagnePink: '#F4DAC6',
+    whiteSmoke: '#F5F5F5',
+    battleShipGrey: '#898989',
+    richBlack: '#13131B',
+    eerieBlack: '#1F2022'
+}
---
+++ src/config/images.js
@@ -0,0 +1,8 @@
+export const images = {
+    logo: require('../assets/images/sicksenseBglessLogo.png'),
+    loginImage: require('../assets/images/loginImage.png'), 
+    sadFace: require('../assets/images/sadImage.png'),
+    happyFace: require('../assets/images/happyImage.png'),
+    doctorAnalysing: require('../assets/images/predictionImage.png'),
+    avatar: require('../assets/images/avatar.png'),
+}
